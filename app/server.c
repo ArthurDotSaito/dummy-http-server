2fa2d87849b9179008f54fca7a0d48fc997b61bf
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-24 20:38:13 -0300

    backuping files

---
+++ app/server.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+char *response_ok = "HTTP/1.1 200 OK\r\n\r\n";
+
+int main()
+{
+	// Disable output buffering
+	setbuf(stdout, NULL);
+
+	int server_fd, client_addr_len;
+	struct sockaddr_in client_addr;
+
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1)
+	{
+		printf("Socket creation failed: %s...\n", strerror(errno));
+		return 1;
+	}
+
+	// Since the tester restarts your program quite often, setting REUSE_PORT
+	// ensures that we don't run into 'Address already in use' errors
+	int reuse = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
+	{
+		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(4221),
+		.sin_addr = {htonl(INADDR_ANY)},
+	};
+
+	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
+	{
+		printf("Bind failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	int connection_backlog = 5;
+	if (listen(server_fd, connection_backlog) != 0)
+	{
+		printf("Listen failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	printf("Waiting for a client to connect...\n");
+	client_addr_len = sizeof(client_addr);
+
+	const int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+	if (client_fd < 0)
+	{
+		printf("Accept failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	printf("Client connected\n");
+
+	char request_buffer[BUFFER_SIZE];
+
+	if (read(client_fd, request_buffer, BUFFER_SIZE) < 0)
+	{
+		printf("Read failed: %s \n", strerror(errno));
+		return 1;
+	}
+	else
+	{
+		printf("Request from client: %s\n", request_buffer);
+	}
+
+	if (send(client_fd, response_ok, strlen(response_ok), 0) < 0)
+	{
+		printf("Error: %s \n", strerror(errno));
+	}
+
+	close(server_fd);
+	close(client_fd);
+
+	return 0;
+}
