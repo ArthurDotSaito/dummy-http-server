cca9b6330cc991ce6b8717390bc99f5aa950bd64
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-26 01:27:07 -0300

    minor refact

--- app/server.c
+++ app/server.c
@@ -7,6 +7,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <asm-generic/socket.h>
 
 #define BUFFER_SIZE 4096
 char *response_ok = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s";
@@ -14,6 +15,49 @@ char *response_ok_unknown_type = "HTTP/1.1 200 OK\r\nContent-Type: application/o
 char *response_not_found = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s";
 char *response_internal_error = "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\n\r\n";
 char *directory;
+
+int init_server(int port);
+void handle_new_connection(int server_fd);
+void send_file_content(int client_fd, const char *file_path);
+void process_client_request(int client_fd);
+
+int init_server(int port)
+{
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1)
+	{
+		printf("Socket creation failed: %s...\n", strerror(errno));
+		return 1;
+	}
+
+	int reuse = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
+	{
+		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(port),
+		.sin_addr = {htonl(INADDR_ANY)},
+	};
+
+	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
+	{
+		printf("Bind failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	int connection_backlog = 5;
+	if (listen(server_fd, connection_backlog) != 0)
+	{
+		printf("Listen failed: %s \n", strerror(errno));
+		return 1;
+	}
+
+	return server_fd;
+}
 
 void send_file_content(int client_fd, const char *file_path)
 {
@@ -53,43 +97,11 @@ int main(int argc, char *argv[])
 
 	directory = argv[2];
 
-	int server_fd, client_addr_len;
+	int client_addr_len;
 	struct sockaddr_in client_addr;
 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1)
-	{
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
-
-	// Since the tester restarts your program quite often, setting REUSE_PORT
-	// ensures that we don't run into 'Address already in use' errors
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
-	{
-		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	struct sockaddr_in serv_addr = {
-		.sin_family = AF_INET,
-		.sin_port = htons(4221),
-		.sin_addr = {htonl(INADDR_ANY)},
-	};
-
-	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
-	{
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0)
-	{
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
+	// Port is hardcoded to 4221
+	int server_fd = init_server(4221);
 
 	printf("Waiting for a client to connect...\n");
 	while (1)
