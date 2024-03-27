e8e9cabc12cb56b4572f2d5d26bda22ae7f853de
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-27 01:09:33 -0300

    [refact] major refact

--- app/server.c
+++ app/server.c
@@ -14,6 +14,7 @@ char *response_ok = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Leng
 char *response_ok_unknown_type = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %ld\r\n\r\n";
 char *response_not_found = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s";
 char *response_internal_error = "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\n\r\n";
+char *response_created = "HTTP/1.1 201 Created\r\nContent-Length: 0\r\n\r\n";
 char *directory;
 
 int init_server(int port);
@@ -57,67 +58,6 @@ int init_server(int port)
 	}
 
 	return server_fd;
-}
-
-void process_client_request(int client_fd)
-{
-	char request_buffer[BUFFER_SIZE];
-	if (read(client_fd, request_buffer, BUFFER_SIZE) < 0)
-	{
-		printf("Read failed: %s \n", strerror(errno));
-		return;
-	}
-
-	printf("Request from client: %s\n", request_buffer);
-
-	char *method = strtok(request_buffer, " ");
-	char *path = strtok(NULL, " ");
-
-	char *headerLine = NULL;
-	char user_agent[BUFFER_SIZE] = {0};
-
-	while ((headerLine = strtok(NULL, "\r\n")) && *headerLine)
-	{
-		if (strncmp(headerLine, "User-Agent:", 11) == 0)
-		{
-			strncpy(user_agent, headerLine + 12, sizeof(user_agent) - 1);
-			break;
-		}
-	}
-
-	if (strcmp(path, "/") == 0)
-	{
-		char response[BUFFER_SIZE];
-		int length = sprintf(response, response_ok, 2, "OK");
-		send(client_fd, response, length, 0);
-	}
-	else if (strncmp(path, "/echo/", 6) == 0)
-	{
-		char *message = path + 6;
-		char response[BUFFER_SIZE];
-		int length = sprintf(response, response_ok, strlen(message), message);
-		send(client_fd, response, length, 0);
-	}
-	else if (strcmp(path, "/user-agent") == 0 && strlen(user_agent) > 0)
-	{
-		char response[BUFFER_SIZE];
-		int length = sprintf(response, response_ok, strlen(user_agent), user_agent);
-		send(client_fd, response, length, 0);
-	}
-	else if (strncmp(path, "/files/", 7) == 0)
-	{
-		char file_path[BUFFER_SIZE];
-		snprintf(file_path, BUFFER_SIZE, "%s%s", directory, path + 6);
-		send_file_content(client_fd, file_path);
-	}
-	else
-	{
-		char response[BUFFER_SIZE];
-		sprintf(response, response_not_found, 13, "File not found");
-		send(client_fd, response, strlen(response), 0);
-	}
-
-	close(client_fd);
 }
 
 void handle_new_connection(int server_fd)
@@ -183,6 +123,144 @@ void send_file_content(int client_fd, const char *file_path)
 	}
 
 	fclose(file);
+}
+
+void handle_post_request(int client_fd, char *path, char *request_buffer, ssize_t bytes_read)
+{
+	char *filename = path + 7; // Skip past "/files/"
+	char full_path[BUFFER_SIZE];
+	snprintf(full_path, sizeof(full_path), "%s/%s", directory, filename);
+
+	printf("Buffer Request: %s\n", request_buffer);
+	long content_length = 0;
+	char *content_length_str_start = strstr(request_buffer, "Content-Length: ");
+	if (content_length_str_start)
+	{
+		content_length = atol(content_length_str_start + 16);
+	}
+
+	FILE *file = fopen(full_path, "w");
+	if (!file)
+	{
+		send(client_fd, response_internal_error, strlen(response_internal_error), 0);
+		return;
+	}
+
+	char *body_start = strstr(request_buffer, "\r\n\r\n") + 4;
+	ssize_t body_read = bytes_read - (body_start - request_buffer);
+	if (body_read > 0 && body_read <= content_length)
+	{
+		fwrite(body_start, 1, body_read, file);
+		content_length -= body_read;
+	}
+
+	printf("Content length: %ld\n", content_length);
+	char buffer[BUFFER_SIZE];
+	while (content_length > 0)
+	{
+		ssize_t bytes_to_read = sizeof(buffer) < content_length ? sizeof(buffer) : content_length;
+		ssize_t read_result = read(client_fd, buffer, bytes_to_read);
+		if (read_result > 0)
+		{
+			fwrite(buffer, 1, read_result, file);
+			content_length -= read_result;
+		}
+		else if (read_result < 0)
+		{
+			printf("Error reading from socket: %s\n", strerror(errno));
+			break;
+		}
+	}
+
+	fclose(file);
+	send(client_fd, response_created, 44, 0);
+}
+
+void process_client_request(int client_fd)
+{
+	char request_buffer[BUFFER_SIZE];
+	memset(request_buffer, 0, sizeof(request_buffer));
+	ssize_t total_bytes_read = 0;
+	ssize_t bytes_read = read(client_fd, request_buffer, BUFFER_SIZE - 1);
+	if (bytes_read < 0)
+	{
+		printf("Read failed: %s\n", strerror(errno));
+		return;
+	}
+	total_bytes_read += bytes_read;
+
+	while (!strstr(request_buffer, "\r\n\r\n") && total_bytes_read < sizeof(request_buffer) - 1)
+	{
+		bytes_read = read(client_fd, request_buffer + total_bytes_read, BUFFER_SIZE - total_bytes_read);
+		if (bytes_read < 0)
+		{
+			printf("Read failed: %s\n", strerror(errno));
+			return;
+		}
+		else if (bytes_read == 0)
+		{
+			break;
+		}
+		total_bytes_read += bytes_read;
+	}
+
+	printf("Request from client: %s\n", request_buffer);
+
+	char request_buffer_copy[BUFFER_SIZE];
+	strcpy(request_buffer_copy, request_buffer);
+
+	char *method = strtok(request_buffer, " ");
+	char *path = strtok(NULL, " ");
+
+	char *headerLine = NULL;
+	char user_agent[BUFFER_SIZE] = {0};
+
+	while ((headerLine = strtok(NULL, "\r\n")) && *headerLine)
+	{
+		if (strncmp(headerLine, "User-Agent:", 11) == 0)
+		{
+			strncpy(user_agent, headerLine + 12, sizeof(user_agent) - 1);
+			break;
+		}
+	}
+
+	if (strcmp(path, "/") == 0)
+	{
+		char response[BUFFER_SIZE];
+		int length = sprintf(response, response_ok, 2, "OK");
+		send(client_fd, response, length, 0);
+	}
+	else if (strncmp(path, "/echo/", 6) == 0)
+	{
+		char *message = path + 6;
+		char response[BUFFER_SIZE];
+		int length = sprintf(response, response_ok, strlen(message), message);
+		send(client_fd, response, length, 0);
+	}
+	else if (strcmp(path, "/user-agent") == 0 && strlen(user_agent) > 0)
+	{
+		char response[BUFFER_SIZE];
+		int length = sprintf(response, response_ok, strlen(user_agent), user_agent);
+		send(client_fd, response, length, 0);
+	}
+	else if (strcmp(method, "POST") == 0 && strncmp(path, "/files/", 7) == 0)
+	{
+		handle_post_request(client_fd, path, request_buffer_copy, bytes_read);
+	}
+	else if (strncmp(path, "/files/", 7) == 0)
+	{
+		char file_path[BUFFER_SIZE];
+		snprintf(file_path, BUFFER_SIZE, "%s%s", directory, path + 6);
+		send_file_content(client_fd, file_path);
+	}
+	else
+	{
+		char response[BUFFER_SIZE];
+		sprintf(response, response_not_found, 13, "File not found");
+		send(client_fd, response, strlen(response), 0);
+	}
+
+	close(client_fd);
 }
 
 int main(int argc, char *argv[])
