dd25f27bf17144fa7aa22f2c276c624646bf0eae
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-26 23:50:27 -0300

    [refact] major refact

--- app/server.c
+++ app/server.c
@@ -59,6 +59,101 @@ int init_server(int port)
 	return server_fd;
 }
 
+void process_client_request(int client_fd)
+{
+	char request_buffer[BUFFER_SIZE];
+	if (read(client_fd, request_buffer, BUFFER_SIZE) < 0)
+	{
+		printf("Read failed: %s \n", strerror(errno));
+		return;
+	}
+
+	printf("Request from client: %s\n", request_buffer);
+
+	char *method = strtok(request_buffer, " ");
+	char *path = strtok(NULL, " ");
+
+	char *headerLine = NULL;
+	char user_agent[BUFFER_SIZE] = {0};
+
+	while ((headerLine = strtok(NULL, "\r\n")) && *headerLine)
+	{
+		if (strncmp(headerLine, "User-Agent:", 11) == 0)
+		{
+			strncpy(user_agent, headerLine + 12, sizeof(user_agent) - 1);
+			break;
+		}
+	}
+
+	if (strcmp(path, "/") == 0)
+	{
+		char response[BUFFER_SIZE];
+		int length = sprintf(response, response_ok, 2, "OK");
+		send(client_fd, response, length, 0);
+	}
+	else if (strncmp(path, "/echo/", 6) == 0)
+	{
+		char *message = path + 6;
+		char response[BUFFER_SIZE];
+		int length = sprintf(response, response_ok, strlen(message), message);
+		send(client_fd, response, length, 0);
+	}
+	else if (strcmp(path, "/user-agent") == 0 && strlen(user_agent) > 0)
+	{
+		char response[BUFFER_SIZE];
+		int length = sprintf(response, response_ok, strlen(user_agent), user_agent);
+		send(client_fd, response, length, 0);
+	}
+	else if (strncmp(path, "/files/", 7) == 0)
+	{
+		char file_path[BUFFER_SIZE];
+		snprintf(file_path, BUFFER_SIZE, "%s%s", directory, path + 6);
+		send_file_content(client_fd, file_path);
+	}
+	else
+	{
+		char response[BUFFER_SIZE];
+		sprintf(response, response_not_found, 13, "File not found");
+		send(client_fd, response, strlen(response), 0);
+	}
+
+	close(client_fd);
+}
+
+void handle_new_connection(int server_fd)
+{
+	struct sockaddr_in client_addr;
+	int client_addr_len = sizeof(client_addr);
+	printf("Waiting for a client to connect...\n");
+	while (1)
+	{
+		int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+		if (client_fd < 0)
+		{
+			printf("Accept failed: %s \n", strerror(errno));
+			continue;
+		}
+
+		int pid = fork();
+
+		if (pid < 0)
+		{
+			printf("Fork failed: %s\n", strerror(errno));
+			close(client_fd);
+		}
+		else if (pid == 0)
+		{
+			close(server_fd);
+			process_client_request(client_fd);
+			exit(0);
+		}
+		else
+		{
+			close(client_fd);
+		}
+	}
+}
+
 void send_file_content(int client_fd, const char *file_path)
 {
 	FILE *file = fopen(file_path, "r");
@@ -100,102 +195,17 @@ int main(int argc, char *argv[])
 	int client_addr_len;
 	struct sockaddr_in client_addr;
 
-	// Port is hardcoded to 4221
+	// Port is binded to 4221
 	int server_fd = init_server(4221);
-
-	printf("Waiting for a client to connect...\n");
-	while (1)
+	if (server_fd < 0)
 	{
-		client_addr_len = sizeof(client_addr);
-
-		int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-		if (client_fd < 0)
-		{
-			printf("Accept failed: %s \n", strerror(errno));
-			continue;
-		}
-
-		int pid = fork();
-		if (pid < 0)
-		{
-			printf("Fork failed: %s\n", strerror(errno));
-			close(client_fd);
-		}
-		else if (pid == 0)
-		{
-			close(server_fd);
-			printf("Client connected\n");
-
-			char request_buffer[BUFFER_SIZE];
-
-			if (read(client_fd, request_buffer, BUFFER_SIZE) < 0)
-			{
-				printf("Read failed: %s \n", strerror(errno));
-				return 1;
-			}
-			else
-			{
-				printf("Request from client: %s\n", request_buffer);
-			}
-
-			char *method = strtok(request_buffer, " ");
-			char *path = strtok(NULL, " ");
-
-			char *headerLine = NULL;
-			char user_agent[BUFFER_SIZE] = {0};
-
-			while ((headerLine = strtok(NULL, "\r\n")) && *headerLine)
-			{
-				if (strncmp(headerLine, "User-Agent:", 11) == 0)
-				{
-					strncpy(user_agent, headerLine + 12, sizeof(user_agent) - 1);
-					break;
-				}
-			}
-
-			char response[BUFFER_SIZE];
-			char body[BUFFER_SIZE] = {0};
-			int length;
-
-			if (strcmp(path, "/") == 0)
-			{
-				length = sprintf(response, response_ok, 2, "OK");
-			}
-			else if (strncmp(path, "/echo/", 6) == 0)
-			{
-				char *message = path + 6;
-				length = sprintf(response, response_ok, strlen(message), message);
-			}
-			else if (strcmp(path, "/user-agent") == 0 && strlen(user_agent) > 0)
-			{
-				length = sprintf(response, response_ok, strlen(user_agent), user_agent);
-			}
-			else if (strncmp(path, "/files/", 7) == 0)
-			{
-				char file_path[BUFFER_SIZE];
-				snprintf(file_path, BUFFER_SIZE, "%s%s", directory, path + 6);
-				send_file_content(client_fd, file_path);
-				close(client_fd);
-				exit(0);
-			}
-			else
-			{
-				length = sprintf(response, response_not_found, 9, body);
-			}
-
-			if (send(client_fd, response, length, 0) < 0)
-			{
-				printf("Send failed: %s\n", strerror(errno));
-			}
-
-			close(client_fd);
-			exit(0);
-		}
-		else
-		{
-			close(client_fd);
-		}
+		printf("Failed to initialize server\n");
+		return 1;
 	}
 
+	printf("Server listening on port %d\n", 4221);
+	handle_new_connection(server_fd);
+
+	close(server_fd);
 	return 0;
 }
